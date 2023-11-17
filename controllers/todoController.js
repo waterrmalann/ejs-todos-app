c7cdfc8ab0ea868115607f8e28a1491ffee97ceb
Alan <56948680+waterrmalann@users.noreply.github.com>
2023-11-18 03:45:11 +0530

    Create todoController.js

---
+++ controllers/todoController.js
@@ -0,0 +1,57 @@
+import Todo from '../models/Todo.js';
+
+const getTodos = async (_req, res) => {
+    try {
+        const todos = await Todo.find();
+        res.status(200).json(todos);
+    } catch (err) {
+        res.status(500).json({ error: err.message });
+    }
+};
+
+const postTodos = async (req, res) => {
+    const { content } = req.body;
+
+    if (!content || content.length < 2) {
+        return res.status(400).json({ error: "no content provided" });
+    }
+
+    try {
+        const newTodo = await Todo.create({ content });
+        res.status(201).json(newTodo);
+    } catch (err) {
+        res.status(500).json({ error: err.message });
+    }
+};
+
+const putTodos = async (req, res) => {
+    const { id } = req.params;
+    const { content, completed } = req.body;
+
+    try {
+        const updatedTodo = await Todo.findByIdAndUpdate(id, { content, completed }, { new: true });
+        if (!updatedTodo) {
+            return res.status(404).json({ message: 'Todo not found' });
+        }
+        res.status(200).json(updatedTodo);
+    } catch (err) {
+        res.status(500).json({ error: err.message });
+    }
+};
+
+const deleteTodos = async (req, res) => {
+    const { id } = req.params;
+
+    try {
+        const deletedTodo = await Todo.findByIdAndDelete(id);
+        if (!deletedTodo) {
+            return res.status(404).json({ message: 'Todo not found' });
+        }
+        res.status(200).json({ message: 'Todo deleted successfully' });
+    } catch (err) {
+        res.status(500).json({ error: err.message });
+    }
+};
+
+
+export { getTodos, postTodos, putTodos, deleteTodos };
