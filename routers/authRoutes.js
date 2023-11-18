65fc569638f67bf6cb185e865c81735c7bb5ce8a
Alan <56948680+waterrmalann@users.noreply.github.com>
2023-11-18 13:18:43 +0530

    fix infinite redirection

--- routers/authRoutes.js
+++ routers/authRoutes.js
@@ -4,15 +4,10 @@ import { getLogin, getLogout, getRegister, postLogin, postRegister } from '../co
 
 const router = express.Router();
 
-// Routes for authentication are not meant to be used if an user is already logged in.
-// A logged in user should have no reason and therefore not be allowed to log in again, or create an account.
-// If they wish to do that, they will have to log out first.
-router.use(redirectIfAlreadyLoggedIn);
-
-router.get('/login', getLogin);
+router.get('/login', redirectIfAlreadyLoggedIn, getLogin);
 router.post('/login', postLogin);
 
-router.get('/register', getRegister);
+router.get('/register', redirectIfAlreadyLoggedIn, getRegister);
 router.post('/register', postRegister);
 
 router.get('/logout', getLogout);
