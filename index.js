04df3645564c13e110a8530329bb31a9d750c47e
Alan <56948680+waterrmalann@users.noreply.github.com>
2023-11-18 12:40:22 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,12 +1,16 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import mongoose from 'mongoose';
+import session from 'express-session';
 
 import authRouter from './routers/authRoutes.js';
 import todoRouter from './routers/todoRoutes.js';
 
 // Inject the `.env` file into `process.env`
 dotenv.config();
+
+// class UserNotSubscribedError extends Error {};
+// throw new UserNotSubscribedError("You are not subscribed to the channel yet.");
 
 // Connect with the database.
 mongoose.connect(process.env.MONGO_URI)
@@ -17,6 +21,7 @@ const app = express();
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(express.static('public'));
 
 app.use(session({
     secret: process.env.SESSION_SECRET,
@@ -25,8 +30,16 @@ app.use(session({
     // cookie: { secure: true }
 }))
 
+app.set('view engine', 'ejs');
+
+// Handle index.
+app.get('/', (req, res) => res.render('index'));
+
 app.use('/', authRouter);
 app.use('/todos', todoRouter);
+
+// Handle our 404.
+app.get('*', (_req, res) => res.render('not-found'));
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
